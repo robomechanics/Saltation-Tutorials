b92f80b9fff4a812e14e1dc8059e8ec1aadefef2
Karla <kcuevas@alumni.princeton.edu>
2024-11-14 10:24:00 -0500

    made code more readable

--- Salted Kalman Filter/Matlab/hybrid_helper_functions/solve_ivp_guard_funcs.m
+++ Salted Kalman Filter/Matlab/hybrid_helper_functions/solve_ivp_guard_funcs.m
@@ -7,21 +7,15 @@ function [guards, new_modes] = solve_ivp_guard_funcs(guards_dict, mode, inputs, 
         guard_keys = fieldnames(guards_dict.(mode));
         for i = 1:length(guard_keys)
             key = guard_keys{i};
-            
-            % Check if parameters are provided for guard function
-            if isempty(parameters)
-                % Create guard function without parameters
-                guard_func = @(t, states) guards_dict.(mode).(key).g(t, states, inputs, dt);
-            else
-                % Create guard function with parameters
-                guard_func = @(t, states) guards_dict.(mode).(key).g(t, states, inputs, dt, parameters);
-            end
-            
+            % Create each individual guard function
+            guard_func = @(t, states) guards_dict.(mode).(key).g(t, states, inputs, dt, parameters);
             guard_funcs{end+1} = guard_func;
             new_modes = [new_modes, key];
         end
     end
 
-    % Output guards as a cell array of guard functions
     guards = guard_funcs;
+    
+    % Combine individual guard functions into a single function handle
+    % guards = @(t, states) cellfun(@(g) g(t, states), guard_funcs);
 end
--- Salted Kalman Filter/Matlab/simple_hybrid_system.m
+++ Salted Kalman Filter/Matlab/simple_hybrid_system.m
@@ -28,20 +28,24 @@ CJ = jacobian(yJ, states);
 
 % Define resets
 rIJ = [x1; x2];  % No change in the reset function in this case
+rJI = [x1; x2];   
 
 % Take the Jacobian of resets with respect to the states
 RIJ = jacobian(rIJ, states);
+RJI = jacobian(rIJ, states);
 
 % Define guard functions
 gIJ = [-x1];  % Switch when x1 < 0
+gJI = [x1 - 10];       % No valid transition from J to I by default
 
 % Take the Jacobian of guards with respect to states
 GIJ = jacobian(gIJ, states);
+GJI = jacobian(gJI, states); % Zero matrix as no guard is active for J to I
 
 % Take the Jacobian of guards with respect to time
 GtIJ = jacobian(gIJ, time);
+GtJI = jacobian(gJI, time);
 
-% Convert symbolic expressions to MATLAB functions
 % Convert symbolic expressions to MATLAB functions
 fI_func = matlabFunction(fI, 'Vars', {states, inputs, dt, params_placeholder});
 AI_disc_func = matlabFunction(AI_disc, 'Vars', {states, inputs, dt, params_placeholder});
@@ -57,31 +61,28 @@ CJ_func = matlabFunction(CJ, 'Vars', {states, inputs, dt, params_placeholder});
 
 rIJ_func = matlabFunction(rIJ, 'Vars', {states, inputs, dt, params_placeholder});
 RIJ_func = matlabFunction(RIJ, 'Vars', {states, inputs, dt, params_placeholder});
+rJI_func = matlabFunction(rJI, 'Vars', {states, inputs, dt, params_placeholder});
+RJI_func = matlabFunction(RJI, 'Vars', {states, inputs, dt, params_placeholder});
 
 gIJ_func = matlabFunction(gIJ, 'Vars', {time, states, inputs, dt, params_placeholder});
 GIJ_func = matlabFunction(GIJ, 'Vars', {states, inputs, dt, params_placeholder});
 GtIJ_func = matlabFunction(GtIJ, 'Vars', {time, states, inputs, dt, params_placeholder});
 
+gJI_func = matlabFunction(gJI, 'Vars', {time, states, inputs, dt, params_placeholder});
+GJI_func = matlabFunction(GJI, 'Vars', {states, inputs, dt, params_placeholder});
+GtJI_func = matlabFunction(GtJI, 'Vars', {time, states, inputs, dt, params_placeholder});
 
-% % Define the dynamics, resets, and guards in struct format
-% dynamics = struct( ...
-%     'I', struct('f_cont', fI_func, 'A_disc', AI_disc_func, 'y', yI_func, 'C', CI_func), ...
-%     'J', struct('f_cont', fJ_func, 'A_disc', AJ_disc_func, 'y', yJ_func, 'C', CJ_func) ...
-% );
-% 
-% resets = struct('I', struct('J', struct('r', rIJ_func, 'R', RIJ_func)));
-% 
-% guards = struct('I', struct('J', struct('g', gIJ_func, 'G', GIJ_func, 'Gt', GtIJ_func)));
-% 
 % Define the dynamics, resets, and guards in struct format
 dynamics = struct( ...
     'I', struct('f_cont', fI_func, 'A_disc', AI_disc_func, 'y', yI_func, 'C', CI_func), ...
     'J', struct('f_cont', fJ_func, 'A_disc', AJ_disc_func, 'y', yJ_func, 'C', CJ_func) ...
 );
 
-resets = struct('I', struct('J', struct('r', rIJ_func, 'R', RIJ_func)));
+resets = struct('I', struct('J', struct('r', rIJ_func, 'R', RIJ_func)), ...
+                'J', struct('I', struct('r', rJI_func, 'R', RJI_func)));
 
-guards = struct('I', struct('J', struct('g', gIJ_func, 'G', GIJ_func, 'Gt', GtIJ_func)));
+guards = struct('I', struct('J', struct('g', gIJ_func, 'G', GIJ_func, 'Gt', GtIJ_func)), ...
+                'J', struct('I', struct('g', gJI_func, 'G', GJI_func, 'Gt', GtJI_func)));
 
 % Define global noise matrices for process and measurement noise
 n_states = 2;
@@ -133,12 +134,14 @@ end
 
 % Plot results1
 figure;
-plot(actual_states(:, 1), actual_states(:, 2), 'k-', 'DisplayName', 'Actual states');
+plot(actual_states(:, 1), actual_states(:, 2), 'k-', 'LineWidth', 2, 'DisplayName', 'Actual states');
 hold on;
-plot(measurements(:, 1), measurements(:, 2), 'r.', 'DisplayName', 'Measurements');
-plot(filtered_states(:, 1), filtered_states(:, 2), 'b--', 'DisplayName', 'Filtered states');
+plot(measurements(:, 1), measurements(:, 2), 'r.', 'MarkerSize', 10, 'DisplayName', 'Measurements');
+plot(filtered_states(:, 1), filtered_states(:, 2), 'b--', 'LineWidth', 1.5, 'DisplayName', 'Filtered states');
 legend;
-xlabel('Position (x1)');
-ylabel('Velocity (x2)');
-title('2D State Space for Hybrid System');
+xlabel('y');
+ylabel('$\dot{y}$', 'Interpreter', 'latex');
+title('1D Bouncing Ball System');
 hold off;
+
+
