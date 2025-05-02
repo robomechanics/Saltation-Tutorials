790213ac89db5c79410f0b2be3528fca512ec857
dfriasfr <94498272+dfriasfr@users.noreply.github.com>
2025-05-02 12:06:36 -0400

    cleaned up tutorial folder

--- Tutorial Paper Code/Backups/Bouncing - Copy/set_params.m
+++
@@ -1,9 +0,0 @@
-function params = set_params()
-
-% add more parameters as necessary, e.g. mass
-params.g = 9.81;    % gravity
-params.alpha = 0.7; % coeff of restitution
-
-% consolidate parameters
-params.parameters = [params.alpha, params.g];
-end
